a4a99595e37ab46f89eec25174ce0822ca9a2eb8
notkhang <145481390+notkhang@users.noreply.github.com>
2024-01-05 16:02:26 +0000

    idk

--- public/main.js
+++ public/main.js
@@ -1,34 +1,17 @@
 // Write JavaScript here.
-console.log('main.js loaded successfully!')
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// Define "showModal" function
-function showModal () {
-  modal.style.display = "block";
+function openAd(event) {
+  event.target.parentElement.querySelector(".modal").classList.add("active");
 }
-
-// When the user clicks the button, open the modal 
-document
-  .querySelector("#myBtn") // grab the element selected by #myBtn
-  .addEventListener("click", showModal); // make clicking run showModal
-
-document.querySelector("h1").onclick = showModal;
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+for (let button of document.querySelectorAll(".ad .trigger")) {
+  button.addEventListener("click", openAd);
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target.classList.contains("ad")) {
+    event.target
+    .querySelector(".modal")
+    .classList.remove("active");
   }
 
 }
